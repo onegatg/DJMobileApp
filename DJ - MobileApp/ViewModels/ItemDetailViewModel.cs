da82590d9fdbc5c6e0d89db72dc0b35fd4b48af3
Samuel Brunner <brunnese@mail.uc.edu>
2019-10-08 18:09:15 -0400

    Add project files.

---
+++ DJ - MobileApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DJ___MobileApp.Models;
+
+namespace DJ___MobileApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
